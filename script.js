888219ec32d6750ff07d38834eaa28f5ae413db5
VitorRekobe <98287250+VitorRekobe@users.noreply.github.com>
2022-10-04 11:27:23 -0300

    Update script.js
    retirando o delay de minuto e segundo

--- script.js
+++ script.js
@@ -6,14 +6,6 @@ var segundos = 60;
 
 var flag_stop = false;
 
-const decMin = () => {
-    minuto = minuto - 1;
-    document.querySelector('#minutes').innerText = minuto;
-    if (minuto == 0 && segundos == 0) {
-        stopp()
-    }
-}
-
 const decSeg = () => {
     segundos = segundos - 1;
     document.querySelector('#seconds').innerText = segundos;
@@ -29,21 +21,18 @@ function start() {
     if (flag_stop == false) {
         minuto = 24;
         segundos = 59;
-    }   else{
+    } else {
         flag_stop = false;
     }
-
 
     document.querySelector('#minutes').innerText = minuto;
     document.querySelector('#seconds').innerText = segundos;
 
     //callback
-    temp_min = setInterval(decMin, 60000) //(função gatilho, tempo(1x10^-3 = 0,001 s -> 1s = 1000ms))
     temp_seg = setInterval(decSeg, 1000)
 }
 
-function clear(){
-    clearInterval(temp_min)
+function clear() {
     clearInterval(temp_seg)
 }
 
@@ -61,4 +50,3 @@ function template() {
     document.querySelector('#seconds').innerText = '00';
     clear();
 }
-
